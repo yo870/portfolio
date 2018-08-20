9f67a42609b32625e722e7c4b5c05c80c8dfd8d4
yo870 <yo870@hotmail.com>
2018-08-20 22:15:04 +0100

    Updated fetch to query API Gateway

--- js/script.js
+++ js/script.js
@@ -34,12 +34,11 @@ document.querySelector("#bcontact").addEventListener("click", () => {
 	});
 });
 
-var messageForm = document.getElementById('messageForm');
+
 
 messageForm.addEventListener('submit', function(event) {
   event.preventDefault();
-  console.log(messageForm[0].value);
-  
+  var messageForm = document.getElementById('messageForm');
   
   // 1. Setup the request
   // ================================
@@ -47,27 +46,22 @@ messageForm.addEventListener('submit', function(event) {
   var headers = new Headers();
   // Tell the server we want JSON back
   headers.set('Accept', 'application/json');
+  
 
-  // 1.2 Form Data
-  // We need to properly format the submitted fields.
-  // Here we will use the same format the browser submits POST forms.
-  // You could use a different format, depending on your server, such
-  // as JSON or XML.
-  var formData = new FormData();
-  for (var i = 0; i < formEl.length; ++i) {
-    formData.append(formEl[i].name, formEl[i].value);
+  var form = {
+    name:messageForm[0].value,
+    email:messageForm[1].value,
+    message:messageForm[2].value
   }
   
-  // This is for the purpose of this demo using jsFiddle AJAX Request endpoint
-  formData.append('json', JSON.stringify({example: 'return value'}));
 
   // 2. Make the request
   // ================================
-  var url = '/echo/json/';
+  var url = 'https://ct7wgzpknj.execute-api.eu-west-2.amazonaws.com/Dev';
   var fetchOptions = {
     method: 'POST',
     headers,
-    body: formData
+    body: JSON.stringify(form)
   };
   
   var responsePromise = fetch(url, fetchOptions);
@@ -82,10 +76,11 @@ messageForm.addEventListener('submit', function(event) {
     // 3.2 Do something with the JSON data
     .then(function(jsonData) {
     	console.log(jsonData);
-      document.getElementById('results').innerText =
-      	JSON.stringify(jsonData);
-    });
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
   
 
-  event.preventDefault();
+  
 });
